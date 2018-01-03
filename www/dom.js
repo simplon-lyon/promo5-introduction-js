0a8e3f69a4b3682581e4c323b8ce49ace5f72f7d
Jean <demeljean@hotmail.fr>
2018-01-03 10:12:08 +0100

    added dom manipulation

---
+++ www/dom.js
@@ -0,0 +1,11 @@
+
+//console.log(document.getElementById('para').textContent);
+
+
+//let para = document.getElementById('para');
+
+let para = document.querySelector('#para');
+
+console.log(para.textContent);
+
+para.textContent = 'bloup';
